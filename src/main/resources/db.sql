6720018f3ea5f990bef7ffc44c06d6889e8f2077
zongxi <zongxiliu@missouristate.edu>
2024-09-04 19:03:59 -0500

    update script

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,36 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 15.3
--- Dumped by pg_dump version 15.3
-
--- Started on 2024-09-04 18:10:30
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE ccdc;
---
--- TOC entry 3330 (class 1262 OID 16402)
--- Name: ccdc; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE ccdc WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'English_United States.1252';
-
-
-ALTER DATABASE ccdc OWNER TO postgres;
-
-\connect ccdc
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
